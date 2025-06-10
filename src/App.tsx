6122783d6ba26ff82999f313b1bee342f4585b4f
Lana Sharin <lana.sharin.webdev@gmail.com>
2025-06-10 10:51:03 -0700

    apply a style to the board (css grid)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import "./App.css";
+import "./css/board.css";
 import Square from "./components/Squares";
 
 function App() {
   const squares = Array.from({ length: 9 }, (_, i) => i.toString());
 
   return (
-    <>
+    <div className="board-container">
       {squares.map((square) => (
         <Square key={square} id={square}/>
       ))}
-    </>
+    </div>
   );
 }
 
