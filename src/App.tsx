8be99c9b72b9f2aa01eb06d9b920b423d06c7a05
Lana Sharin <lana.sharin.webdev@gmail.com>
2025-06-10 11:32:48 -0700

    change the winner type + check if we have got a win combination on the boardValues update + set the winner

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,33 @@ function App() {
   const squares = Array.from({ length: 9 }, (_, i) => i.toString());
   const [boardValues, setBoardValues] = useState<string[]>(Array(9).fill(""));
   const [playerTurn, setPlayerTurn] = useState<boolean>(false);
-  const [winner, setWinner] = useState(false);
+  const [winner, setWinner] = useState<string>("");
 
   useEffect(() => {
     const winCombinations = [
       // rows
-      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
       // cols
-      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
       // diagonals
-      [0, 4, 8], [2, 4, 6]
-    ]
-  }, [boardValues])
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let [a, b, c] of winCombinations) {
+      if (
+        boardValues[a] &&
+        boardValues[a] === boardValues[b] &&
+        boardValues[a] === boardValues[c]
+      ) {
+        setWinner(boardValues[a]);
+      }
+    }
+  }, [boardValues]);
 
   const handleTurns = (cellVal: string, index: number): void => {
     const updatedBoard = [...boardValues];
