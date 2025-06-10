43bceecf896e8948bf49878f3ba3468e52bd9065
Lana Sharin <lana.sharin.webdev@gmail.com>
2025-06-10 11:10:53 -0700

    update values of the clicked board cells to X or O

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import {useState} from "react";
+import { useState } from "react";
 import "./css/board.css";
 import Square from "./components/Squares";
 
 function App() {
   const squares = Array.from({ length: 9 }, (_, i) => i.toString());
-  const [boardValues, setBoardValues] = useState(Array(9).fill(""));
+  const [boardValues, setBoardValues] = useState<string[]>(Array(9).fill(""));
+  const [playerTurn, setPlayerTurn] = useState<boolean>(false);
+
+  const handleTurns = (cellVal: string, index: number): void => {
+    const updatedBoard = [...boardValues];
+    updatedBoard[index] = cellVal;
+    setBoardValues(updatedBoard);
+    setPlayerTurn((prev) => !prev);
+  };
 
   return (
     <div className="board-container">
       {squares.map((square, idx) => (
-        <Square key={square} id={square} value={boardValues[idx]} />
+        <Square
+          key={square}
+          id={square}
+          value={boardValues[idx]}
+          playerTurn={playerTurn}
+          onHandleTurns={(cellValue: string) => handleTurns(cellValue, idx)}
+        />
       ))}
     </div>
   );
--- src/components/Squares.tsx
+++ src/components/Squares.tsx
@@ -1,9 +1,15 @@
 interface SquareInterface {
   id: string;
   value: string;
+  playerTurn: boolean;
+  onHandleTurns: (cellVal: string) => void;
 }
-const Square = ({ id, value }: SquareInterface) => {
-  return <button type="button">{value}</button>;
+const Square = ({ id, value, playerTurn, onHandleTurns }: SquareInterface) => {
+  const handleClick= () => {
+    const cellValue = playerTurn ? "O" : "X";
+    onHandleTurns(cellValue);
+  }
+  return <button type="button" onClick={handleClick}>{value}</button>;
 };
 
 export default Square;
